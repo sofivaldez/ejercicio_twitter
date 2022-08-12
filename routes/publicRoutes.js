3bac8c8c3ff8645e04ac3385edd4ce467a52e3aa
JuanZubillaga <juanzubillagap@gmail.com>
2022-08-12 16:51:28 -0300

    cambios en el register

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const publicRouter = express.Router();
 const authController = require("../controllers/authController");
+const { User } = require("../models");
 
 publicRouter.get("/", (req, res) => {
   res.render("welcome");
@@ -12,6 +13,19 @@ publicRouter.post("/login", authController.login);
 
 publicRouter.get("/register", (req, res) => {
   res.render("register");
+});
+
+publicRouter.post("/register", (req, res) => {
+  const user = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastName,
+    username: req.body.userName, //estaría bueno que se correspondiera con el firstname, lastname
+    email: req.body.email,
+    password: bcrypt.hash(req.body.password, 8), //idealmente en el modelo, un hook
+    bio: req.body.bio,
+    avatar: "default.png",
+  });
+  res.send("Te has registrado correctamente");
 });
 
 module.exports = publicRouter;
